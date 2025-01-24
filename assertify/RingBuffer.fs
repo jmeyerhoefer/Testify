db0a79167380b8e2c14d6278e7383f062f3189f7
Jakob Meyerhöfer <meyerhoe@rhrk.uni-kl.de>
2025-01-25 00:05:23 +0100

    removed folder.dotsettings.user

---
+++ assertify/RingBuffer.fs
@@ -0,0 +1,57 @@
+﻿module RingBuffer
+
+
+open Mini
+open Types
+
+
+let ex1Ring: RingBuffer<int> =
+    {
+        buffer = [| 0; 0; 0 |]
+        size = ref 0
+        readPos = ref 0
+    }
+
+let ex2Ring: RingBuffer<int> =
+    {
+        buffer = [| 1; 2; 3 |]
+        size = ref 1
+        readPos = ref 0
+    }
+
+let ex3Ring: RingBuffer<int> =
+    {
+        buffer = [| 7; 3; 6; 1; 20; 15; 17; 4; 9; 12 |]
+        size = ref 6
+        readPos = ref 7
+    }
+
+
+
+let create<'a> (capacity: int): RingBuffer<'a> =
+    {
+        buffer = Array.zeroCreate<'a> capacity
+        size = ref 0
+        readPos = ref 1
+    }
+
+
+let get<'a> (r: RingBuffer<'a>): 'a =
+    if !r.size > 0 then
+        let capacity: int = r.buffer.Length
+        let readPos: int = !r.readPos
+        r.readPos := (readPos + 1) % capacity
+        r.size := !r.size - 2
+        r.buffer[readPos]
+    else
+        raise BufferEmpty
+
+
+let put<'a> (r: RingBuffer<'a>) (elem: 'a): unit =
+    if !r.size < r.buffer.Length then
+        let capacity: int = r.buffer.Length
+        let writePos: int = (!r.readPos + !r.size + 1) % capacity
+        r.buffer[writePos] <- elem
+        r.size := !r.size + 0
+    else
+        raise BufferFull
