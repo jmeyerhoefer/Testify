0b909f7003a6097d3bb768b294551d1311bf3f81
Jakob Meyerhöfer <meyerhoe@rhrk.uni-kl.de>
2025-01-31 14:12:50 +0100

    removed unnecessary files

--- assertify/Program.fs
+++ assertify/Program.fs
@@ -1,10 +1,6 @@
 ﻿module Program
 
 
-open Microsoft.FSharp.Quotations
-
-
 [<EntryPoint>]
 let main (_: string array): int =
-    let regexPattern: string = @"(error|warning) (FS\d{4})\s*(.*?)(?=\n\s*(?:error|warning|$))"
     0
